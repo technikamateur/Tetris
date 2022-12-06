d3b7c08c34dbe057892da835ec46d23163fb36f2
technikamateur <daniel@technikamateur.de>
2022-12-06 17:51:18 +0100

    added background run

--- ultimative_bencher.sh
+++ ultimative_bencher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 benchmarks=("IS" "FT")
-classes=("B" "C")
+classes=("B")
 
 for bench in ${benchmarks[@]}; do
     for class in ${classes[@]}; do
@@ -10,5 +10,15 @@ for bench in ${benchmarks[@]}; do
 done
 
 for f in ./bin/*.x; do
-    /usr/bin/time -f %U,%S,%e -o $f.time perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$f.txt
+    /usr/bin/time -f %U,%S,%e -o $f.time perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$f.txt &
+    while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
+        sleep 0.5
+    done
+
+    sleep 0.5
+    echo 1 > set_cores.pipe
+
+    while [ -p set_cores.pipe -a -p set_threads.pipe ]; do
+        sleep 1
+    done
 done
