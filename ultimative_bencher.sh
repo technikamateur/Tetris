669175b2113ecaa4fd4f493739d370bb29b049f4
technikamateur <daniel@technikamateur.de>
2022-12-13 13:00:03 +0100

    better structure for results

--- ultimative_bencher.sh
+++ ultimative_bencher.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
 benchmarks=("IS" "FT" "EP" "CG" "MG")
-classes=("A")
+classes=("B")
 cores=("1" "4")
 threads=("1")
 
-for bench in ${benchmarks[@]}; do
-    for class in ${classes[@]}; do
-        make $bench CLASS=$class
+if [[ ! -d "bin" ]]; then
+    mkdir bin
+    for bench in ${benchmarks[@]}; do
+        for class in ${classes[@]}; do
+            make $bench CLASS=$class
+        done
     done
-done
+fi
+
+currentDate=`date +"%Y-%m-%d-%H-%M-%S"`
+
+if [[ ! -d "results" ]]; then
+    mkdir results
+fi
+mkdir ./results/$currentDate
 
 for f in ./bin/*.x; do
+    bname=./results/$currentDate/${f##*/}
     for core in ${cores[@]}; do
-        /usr/bin/time -f %U,%S,%e -o $f.CORES$core.time perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$f.CORES$core.txt &
+        /usr/bin/time -f %U,%S,%e -o $bname.CORES$core.time perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$bname.CORES$core.txt &
         while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
             sleep 0.1
         done
@@ -25,7 +36,7 @@ for f in ./bin/*.x; do
     done
 
     for thr in ${threads[@]}; do
-        /usr/bin/time -f %U,%S,%e -o $f.THR$thr.time perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$f.THR$thr.txt &
+        /usr/bin/time -f %U,%S,%e -o $bname.THR$thr.time perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$bname.THR$thr.txt &
         while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
             sleep 0.5
         done
