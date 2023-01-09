ed3daa77f62e3de3aeb007e5c6464bcb8026c928
technikamateur <daniel@technikamateur.de>
2023-01-09 15:34:51 +0100

    improved bash script - still has bugs

--- ultimative_bencher.sh
+++ ultimative_bencher.sh
@@ -5,6 +5,7 @@ classes=("B")
 cores=("4" "1")
 threads=("1")
 
+# compile if needed
 if [[ ! -d "bin" ]]; then
     mkdir bin
     for bench in ${benchmarks[@]}; do
@@ -14,19 +15,27 @@ if [[ ! -d "bin" ]]; then
     done
 fi
 
-currentDate=`date +"%Y-%m-%d-%H-%M-%S"`
-
 if [[ ! -d "results" ]]; then
     mkdir results
 fi
+
+currentDate=`date +"%Y-%m-%d-%H-%M-%S"`
 mkdir ./results/$currentDate
+
+perfParanoia=`cat /proc/sys/kernel/perf_event_paranoid`
+if [[ perfParanoia -ne "-1" ]]; then
+    echo "Perf is too paranoid."
+    exit 1
+fi
+
+export WAIT_FOR_PIPE=1
 
 for f in ./bin/*.x; do
     bname=./results/$currentDate/${f##*/}
     for core in ${cores[@]}; do
         /usr/bin/time -f %U,%S,%e perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>>$bname.CORES$core.txt &
         while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
-            sleep 0.1
+            sleep 1
         done
         echo $core > set_cores.pipe
 
@@ -38,7 +47,7 @@ for f in ./bin/*.x; do
     for thr in ${threads[@]}; do
         /usr/bin/time -f %U,%S,%e perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>$bname.THR$thr.txt &
         while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
-            sleep 0.1
+            sleep 1
         done
         echo $thr > set_threads.pipe
 
