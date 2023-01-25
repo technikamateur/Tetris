c19352a1adbc4885de08bbd98e3d662288b2e2c7
Daniel <daniel@technikamateur.de>
2023-01-25 14:02:36 +0100

    plots for every bench

--- extractor.py
+++ extractor.py
@@ -10,32 +10,38 @@ delimiter = "#"
 result_dir = max(glob.glob(os.path.join(result_dir, '*/')), key=os.path.getmtime)
 # some generic setup
 bench_dict = dict()
-
+# PANDAS?!
+#https://matplotlib.org/3.4.3/gallery/ticks_and_spines/multiple_yaxis_with_spines.html
 
 class Bench:
     def __init__(self, cores: int, threads: int):
         self.threads = threads
         self.cores = cores
-        self.energy_pkg = 0
-        self.energy_cores = 0
-        self.user = 0
-        self.sys = 0
-        self.execution = 0
+        self.energy_pkg = list()
+        self.energy_cores = list()
+        self.user = list()
+        self.sys = list()
+        self.execution = list()
 
     def __str__(self):
         return f"{self.cores} Cores, {self.threads} Threads"
 
     def populate_from_file(self, file_path: str) -> None:
         with open(file_path, "r") as result:
-            energy_pkg = result.readline().rstrip()
-            energy_cores = result.readline().rstrip()
-            time = result.readline().rstrip()
-        self.energy_pkg = float(energy_pkg.split(",")[0])
-        self.energy_cores = float(energy_cores.split(",")[0])
-        time_split = time.split(",")
-        self.user = float(time_split[0])
-        self.sys = float(time_split[1])
-        self.execution = float(time_split[2])
+            line = result.readline().rstrip()
+            while line:
+                energy_pkg = line
+                energy_cores = result.readline().rstrip()
+                time = result.readline().rstrip()
+                self.energy_pkg.append(float(energy_pkg.split(",")[0]))
+                self.energy_cores.append(float(energy_cores.split(",")[0]))
+                time_split = time.split(",")
+                self.user.append(float(time_split[0]))
+                self.sys.append(float(time_split[1]))
+                self.execution.append(float(time_split[2]))
+
+                # is there more? -> repetitions
+                line = result.readline().rstrip()
         return
 
 
@@ -49,16 +55,20 @@ for file in os.scandir(result_dir):
 
 
 for name, benchs in bench_dict.items():
+    print("Benchmark {}".format(name))
     plt.style.use('ggplot')
     fig, ax = plt.subplots()
     x_axes, y_user, y_sys, y_exe = (list() for i in range(4))
     for b in benchs:
         x_axes.append(str(b.cores) + "/" + str(b.threads))
-        y_user.append(b.user)
-        y_sys.append(b.sys)
-        y_exe.append(b.execution)
+        y_user.append(b.user[0])
+        y_sys.append(b.sys[0])
+        y_exe.append(b.execution[0])
+    ax.set_ylabel("time in seconds")
+    ax.set_xlabel("Cores/Threads")
     ax.plot(x_axes, y_user, 'o-', label="user time")
     ax.plot(x_axes, y_sys, 'o-', label="sys time")
     ax.plot(x_axes, y_exe, 'o-', label="exe time")
-    fig.savefig("pics/{}.svg".format(key))
+    plt.legend()
+    fig.savefig("pics/{}.svg".format(name))
     plt.close()
