8fafad433e6c9471143aa5d5e1a92e7b13bbd402
Daniel <daniel@technikamateur.de>
2023-02-06 16:29:34 +0100

    update bench script

--- ultimative_bencher.sh
+++ ultimative_bencher.sh
@@ -4,8 +4,7 @@
 benchmarks=("IS" "FT" "EP" "CG" "MG")
 classes=("A" "B" "C")
 cores=("8" "4" "2" "1")
-threads=("4" "2" "1")
-max_thread=8
+threads=("8" "4" "2" "1")
 ###
 
 # compile if needed
@@ -44,45 +43,20 @@ export WAIT_FOR_PIPE=1
 for f in ./bin/*.x; do
     bname=./results/$currentDate/${f##*/}
     for i in {1..5}; do
-        #modify cores - max threads
         for core in ${cores[@]}; do
-            /usr/bin/time -f %U,%S,%e perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>>$bname#$core,$max_thread.txt &
-            while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
-                sleep 1
-            done
-            echo $core > set_cores.pipe
+            for thr in ${threads[@]}; do
+                /usr/bin/time -f %U,%S,%e perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>>$bname#$core,$thr.txt &
+                while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
+                    sleep 1
+                done
 
-            while [ -p set_cores.pipe -a -p set_threads.pipe ]; do
-                sleep 1
-            done
-        done
+                echo $thr > set_cores.pipe
+                echo $thr > set_threads.pipe
 
-        #modify threads - max cores
-        for thr in ${threads[@]}; do
-            /usr/bin/time -f %U,%S,%e perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>>$bname#$max_thread,$thr.txt &
-            while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
-                sleep 1
-            done
-            echo $thr > set_threads.pipe
-
-            while [ -p set_cores.pipe -a -p set_threads.pipe ]; do
-                sleep 1
+                while [ -p set_cores.pipe -a -p set_threads.pipe ]; do
+                    sleep 1
+                done
             done
         done
-
-        #modify both
-        for thr in ${threads[@]}; do
-            /usr/bin/time -f %U,%S,%e perf stat --field-separator , -e energy-pkg,energy-cores env LD_PRELOAD=./is_it_openmp.so $f 2>>$bname#$thr,$thr.txt &
-            while [ ! -p set_cores.pipe -a ! -p set_threads.pipe ]; do
-                sleep 1
-            done
-            echo $thr > set_cores.pipe
-            echo $thr > set_threads.pipe
-
-            while [ -p set_cores.pipe -a -p set_threads.pipe ]; do
-                sleep 1
-            done
-        done
-
     done
 done
